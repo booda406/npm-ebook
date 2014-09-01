6cc3a38d5885b9f5943dabf75f7586ee42d39563
Steven Tseng <booda406@gmail.com>
2014-09-01 11:52:27 +0800

    add new js

--- app/controllers/epub_controller.rb
+++ app/controllers/epub_controller.rb
@@ -51,16 +51,16 @@ class EpubController < ApplicationController
 		# 	# nav 'text/toc.xhtml'
 
 		#     ordered {
-		#       @book.add_item('second.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>Second</title></head><body><p>天氣:'+@@epub.second_weather+'</p><p>心情:'+@@epub.second_mood+'</p><p>朋友:'+@@epub.second_friend+'</p></body></html>'))
+		#       @book.add_item('second.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>Second</title></head><body><p>天氣:'+@@epub.second_weather+'</p><p>心情:'+@@epub.second_mood+'</p><p>朋友:'+@@epub.second_friend+'</p></body></html>'))
 		#       @@epub.pages.each_with_index do |page, index|
 		#       	if page.image.url.nil?
-		# 			@book.add_item('chap'+ index.to_s + '.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>'+ page.title + '</title></head><body><p>'+ page.word + '</p></body></html>'))
+		# 			@book.add_item('chap'+ index.to_s + '.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>'+ page.title + '</title></head><body><p>'+ page.word + '</p></body></html>'))
 		#       	else
-		# 			@book.add_item('chap'+ index.to_s + '.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>'+ page.title + '</title></head><body><p>'+ page.word + '</p><img src="public' + page.image.url + '" /></body></html>'))
+		# 			@book.add_item('chap'+ index.to_s + '.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>'+ page.title + '</title></head><body><p>'+ page.word + '</p><img src="public' + page.image.url + '" /></body></html>'))
 		# 		end
 		#       end
-		#       @book.add_item('together_photo.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>TogetherPhoto</title></head><body><img src="public'+@@epub.together_photo.url+'" /></body></html>'))
-		#       @book.add_item('last_image.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>LastImage</title></head><body><img src="public'+@@epub.last_image.url+'" /><p>作者:'+@@epub.creator+'</p><p>時間:'+Time.now.to_s+'</p></body></html>'))
+		#       @book.add_item('together_photo.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>TogetherPhoto</title></head><body><img src="public'+@@epub.together_photo.url+'" /></body></html>'))
+		#       @book.add_item('last_image.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>LastImage</title></head><body><img src="public'+@@epub.last_image.url+'" /><p>作者:'+@@epub.creator+'</p><p>時間:'+Time.now.to_s+'</p></body></html>'))
 		#       # file 'text/chap1.xhtml'
 		#       # heading 'Chapter 1'
 
@@ -107,22 +107,16 @@ class EpubController < ApplicationController
 			# nav 'text/toc.xhtml'
 
 		    ordered {
-		      @book.add_item('second.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>Second</title></head><body><p>天氣:'+@@epub.second_weather+'</p><p>心情:'+@@epub.second_mood+'</p><p>朋友:'+@@epub.second_friend+'</p></body></html>'))
+		      @book.add_item('second.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>Second</title></head><body><p>天氣:'+@@epub.second_weather+'</p><p>心情:'+@@epub.second_mood+'</p><p>朋友:'+@@epub.second_friend+'</p></body></html>'))
 		      @@epub.pages.each_with_index do |page, index|
 		      	if page.image.url.nil?
-					@book.add_item('chap'+ index.to_s + '.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>'+ page.title + '</title></head><body><p>'+ page.word + '</p></body></html>'))
+					@book.add_item('chap'+ index.to_s + '.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>'+ page.title + '</title></head><body><p>'+ page.word + '</p></body></html>'))
 		      	else
-					@book.add_item('chap'+ index.to_s + '.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>'+ page.title + '</title></head><body><p>'+ page.word + '</p><img src="public' + page.image.url + '" /></body></html>'))
+					@book.add_item('chap'+ index.to_s + '.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>'+ page.title + '</title></head><body><p>'+ page.word + '</p><img src="public' + page.image.url + '" /></body></html>'))
 				end
 		      end
-		      @book.add_item('together_photo.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>TogetherPhoto</title></head><body><img src="public'+@@epub.together_photo.url+'" /></body></html>'))
-		      @book.add_item('last_image.xhtml').add_content(StringIO.new('<html xmlns="http://www.w3.org/1999/xhtml"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>LastImage</title></head><body><img src="public'+@@epub.last_image.url+'" /><p>作者:'+@@epub.creator+'</p><p>時間:'+Time.now.to_s+'</p></body></html>'))
-		      # file 'text/chap1.xhtml'
-		      # heading 'Chapter 1'
-
-		      # file 'text/chap1-1.xhtml'
-
-		      # file 'text/chap2.xhtml'
+		      @book.add_item('together_photo.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>TogetherPhoto</title></head><body><img src="public'+@@epub.together_photo.url+'" /></body></html>'))
+		      @book.add_item('last_image.xhtml').add_content(StringIO.new('<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><link rel="stylesheet" type="text/css" href="app/controllers/css/stylesheet.css" /><title>LastImage</title></head><body><img src="public'+@@epub.last_image.url+'" /><p>作者:'+@@epub.creator+'</p><p>時間:'+Time.now.to_s+'</p></body></html>'))
 		      file 'app/controllers/text/chap2-1.xhtml'
 		    }
 		  }
@@ -133,39 +127,39 @@ class EpubController < ApplicationController
 	end
 
 	def generate_ebook
-		builder = GEPUB::Builder.new {
-		  language 'en'
-		  unique_identifier 'http:/example.jp/bookid_in_url', 'BookID', 'URL'
-		  title '故宮測試'
-		  subtitle '我只為了測試'
+		# builder = GEPUB::Builder.new {
+		#   language 'en'
+		#   unique_identifier 'http:/example.jp/bookid_in_url', 'BookID', 'URL'
+		#   title '故宮測試'
+		#   subtitle '我只為了測試'
 
-		  creator 'Steven'
+		#   creator 'Steven'
 
-		  contributors 'Robert', 'Peter'
+		#   contributors 'Robert', 'Peter'
 
-		  date Time.now
+		#   date Time.now
 
-		  resources(:workdir => "#{Rails.root}/app/controllers/") {
- 			glob 'img/*.jpg' # means files(Dir.glob('img/*.jpg'))
-		    cover_image 'img/image1.jpg'
-		    file 'css/stylesheet.css'
-		    glob 'fonts/*.otf'
+		#   resources(:workdir => "#{Rails.root}/app/controllers/") {
+ 	# 		glob 'img/*.jpg' # means files(Dir.glob('img/*.jpg'))
+		#     cover_image 'img/image1.jpg'
+		#     file 'css/stylesheet.css'
+		#     glob 'fonts/*.otf'
  
-			# nav 'text/toc.xhtml'
+		# 	# nav 'text/toc.xhtml'
 
-		    ordered {
-		      file 'text/chap1.xhtml'
-		      heading 'Chapter 1'
+		#     ordered {
+		#       file 'text/chap1.xhtml'
+		#       heading 'Chapter 1'
 
-		      file 'text/chap1-1.xhtml'
+		#       file 'text/chap1-1.xhtml'
 
-		      file 'text/chap2.xhtml'
-		      file 'text/chap2-1.xhtml'
-		    }
-		  }
-		}
-		epubname = File.join("#{Rails.root}/public", '故宮.epub')
-		builder.generate_epub(epubname)
+		#       file 'text/chap2.xhtml'
+		#       file 'text/chap2-1.xhtml'
+		#     }
+		#   }
+		# }
+		# epubname = File.join("#{Rails.root}/public", '故宮.epub')
+		# builder.generate_epub(epubname)
 		send_file("#{Rails.root}/public/故宮.epub")
 	end
 
